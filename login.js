3c9bcc3216e496cc01692de91fac3606f9f83cd8
nayasuna <nayasuna01@students.amikom.ac.id>
2023-10-15 22:43:43 +0700

    menambahkan login

---
+++ login.js
@@ -0,0 +1,14 @@
+function func() {
+    var username = document.getElementById("Username/Email").value;
+    var pass = document.getElementById("Password").value;
+  
+    if (username == 'skilhealth01' && pass == '12345'){
+      alert("Succesfull Login")
+      window.location.assign("home.html")
+    }
+    else{
+      alert("Login Invalid")
+    }
+  }
+  
+  
